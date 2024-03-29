9d0aedf673e8dada273f56c784769ed1e72a5e59
Sidroded <deinekindaniil21@gmail.com>
2024-03-29 17:53:15 +0100

    feat: Add cookie

--- src/main/java/com/sidroded/web/TimeServlet.java
+++ src/main/java/com/sidroded/web/TimeServlet.java
@@ -1,18 +1,19 @@
 package com.sidroded.web;
 
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JavaxServletWebApplication;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -22,10 +23,11 @@ public class TimeServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
+        JavaxServletWebApplication jswa = JavaxServletWebApplication.buildApplication(this.getServletContext());
         engine = new TemplateEngine();
 
-        FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setPrefix("./templates/");
+        WebApplicationTemplateResolver resolver = new WebApplicationTemplateResolver(jswa);
+        resolver.setPrefix("WEB-INF/temp/");
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(engine.getTemplateResolvers().size());
@@ -33,25 +35,52 @@ public class TimeServlet extends HttpServlet {
         engine.addTemplateResolver(resolver);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String timezoneParam = request.getParameter("timezone");
         ZoneId zoneId;
+        Cookie[] cookies = request.getCookies();
+        String lastTimezoneCookie = getLastTimezoneCookie(cookies);
 
         if (timezoneParam != null && !timezoneParam.isEmpty()) {
             timezoneParam = timezoneParam.replace(" ", "+");
             zoneId = ZoneId.of(timezoneParam);
+            setLastTimezoneCookie(timezoneParam, response);
+        } else if (lastTimezoneCookie != null) {
+            zoneId = ZoneId.of(lastTimezoneCookie);
         } else {
-            zoneId = ZoneOffset.UTC;
+            zoneId = ZoneId.of("UTC+0");
         }
 
+        String currentTime = getCurrentTime(zoneId);
+
+        Context ctx = new Context();
+        ctx.setVariable("currentTime", currentTime);
+        engine.process("time", ctx, response.getWriter());
+    }
+
+    private void setLastTimezoneCookie(String timezone, HttpServletResponse resp) {
+        if (timezone != null && !timezone.isEmpty()) {
+            Cookie lastTimezone = new Cookie("timezone", timezone);
+            resp.addCookie(lastTimezone);
+        }
+    }
+
+    private String getLastTimezoneCookie(Cookie[] cookies) {
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("timezone".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getCurrentTime(ZoneId zoneId) {
         ZonedDateTime time = ZonedDateTime.now(zoneId);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
-        String currentTime = formatter.format(time);
-
-        WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale());
-        ctx.setVariable("currentTime", currentTime);
-
-        engine.process("time", ctx, response.getWriter());
+        return formatter.format(time);
     }
 
 }
